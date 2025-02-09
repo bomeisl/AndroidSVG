6e572a3debe441805525d1a95021fe9d8806b3a2
Kyle Bomeisl <ky.bomeisl@gmail.com>
2025-02-09 04:49:43 -0500

    Created XML parser class to deserialize SVGs. Created PathNodeBuilderClass to convert parsed path data into path nodes to build ImageVectors

---
+++ androidsvg/src/main/java/com/kbomeisl/androidsvg/PathNodeBuilder.kt
@@ -0,0 +1,208 @@
+package com.kbomeisl.androidsvg.parsing
+
+import androidx.compose.ui.graphics.vector.PathNode
+
+object PathNodeBuilder {
+
+    fun buildLineNode(x: Float, y: Float): PathNode {
+        val lineTo = PathNode.LineTo(
+            x = x,
+            y = y
+        )
+        return lineTo
+    }
+
+    fun buildMoveTo(x: Float, y: Float): PathNode {
+        val moveTo = PathNode.MoveTo(
+            x = x,
+            y = y
+        )
+        return moveTo
+    }
+
+    fun buildVerticalLine(y: Float): PathNode {
+        val verticalTo = PathNode.VerticalTo(
+            y = y
+        )
+        return verticalTo
+    }
+
+    fun buildHorizontalLine(x: Float): PathNode {
+        val horizontalTo = PathNode.HorizontalTo(
+            x = x
+        )
+        return horizontalTo
+    }
+
+    fun BuildClosePath(): PathNode {
+        return PathNode.Close
+    }
+
+    fun relativeHorizontal(dx: Float): PathNode {
+        val relativeHorizontal = PathNode.RelativeHorizontalTo(
+            dx = dx
+        )
+        return relativeHorizontal
+    }
+
+    fun buildRelativeVertical(dy: Float): PathNode {
+        val relativeVertical = PathNode.RelativeVerticalTo(
+            dy = dy
+        )
+        return relativeVertical
+    }
+
+    fun buildRelativeLineTo(dx: Float, dy: Float): PathNode {
+        val relativeLineTo = PathNode.RelativeLineTo(
+            dx = dx,
+            dy = dy
+        )
+        return relativeLineTo
+    }
+
+    fun buildCubicCurve(
+        x1: Float,
+        y1: Float,
+        x2: Float,
+        y2: Float,
+        x3: Float,
+        y3: Float
+    ): PathNode {
+        val cubicCurve = PathNode.CurveTo(
+            x1 = x1,
+            y1 = y1,
+            x2 = x2,
+            y2 = y2,
+            x3 = x3,
+            y3 = y3
+        )
+        return cubicCurve
+    }
+
+    fun buildQuadraticCurve(
+        x1: Float,
+        y1: Float,
+        x2: Float,
+        y2: Float,
+    ): PathNode {
+        val quadraticCurve = PathNode.QuadTo(
+            x1 = x1,
+            y1 = y1,
+            x2 = x2,
+            y2 = y2
+        )
+        return quadraticCurve
+    }
+
+    fun buildRelativeCubicCurve(
+        dx1: Float,
+        dy1: Float,
+        dx2: Float,
+        dy2: Float,
+        dx3: Float,
+        dy3: Float
+    ): PathNode {
+        val relativeCubicCurve = PathNode.RelativeCurveTo(
+            dx1 = dx1,
+            dy1 = dy1,
+            dx2 = dx2,
+            dy2 = dy2,
+            dx3 = dx3,
+            dy3 = dy3
+        )
+        return relativeCubicCurve
+    }
+
+    fun buildRelativeQuadraticCurve(
+        dx1: Float,
+        dy1: Float,
+        dx2: Float,
+        dy2: Float,
+    ): PathNode {
+        val relativeQuadraticCurve = PathNode.RelativeQuadTo(
+            dx1 = dx1,
+            dy1 = dy1,
+            dx2 = dx2,
+            dy2 = dy2
+        )
+        return relativeQuadraticCurve
+    }
+
+    fun buildArc(
+        rx: Float,
+        ry: Float,
+        xAxisRotation: Float,
+        largeArcFlag: Boolean,
+        sweepFlag: Boolean,
+        x: Float,
+        y: Float
+    ): PathNode {
+        val arc = PathNode.ArcTo(
+            horizontalEllipseRadius = rx,
+            verticalEllipseRadius = ry,
+            theta = xAxisRotation,
+            isMoreThanHalf = sweepFlag,
+            isPositiveArc = largeArcFlag,
+            arcStartX = x,
+            arcStartY = y
+        )
+        return arc
+    }
+
+    fun buildRelativeArc(
+        rx: Float,
+        ry: Float,
+        xAxisRotation: Float,
+        largeArcFlag: Boolean,
+        sweepFlag: Boolean,
+        dx: Float,
+        dy: Float
+    ): PathNode {
+        val arc = PathNode.RelativeArcTo(
+            horizontalEllipseRadius = rx,
+            verticalEllipseRadius = ry,
+            theta = xAxisRotation,
+            isMoreThanHalf = sweepFlag,
+            isPositiveArc = largeArcFlag,
+            arcStartDx = dx,
+            arcStartDy = dy
+        )
+        return arc
+    }
+
+    fun buildReflectionCubicCurve(x1: Float, y1: Float, x2: Float, y2: Float): PathNode {
+        val reflectedCubicCurve = PathNode.ReflectiveCurveTo(
+            x1 = x1,
+            y1 = y1,
+            x2 = x2,
+            y2 = y2
+        )
+        return reflectedCubicCurve
+    }
+
+    fun buildRelativeReflectiveCubicCurve(dx1: Float, dy1: Float, dx2: Float, dy2: Float): PathNode {
+        val relativeReflectedCubicCurve = PathNode.RelativeReflectiveCurveTo(
+            dx1 = dx1,
+            dy1 = dy1,
+            dx2 = dx2,
+            dy2 = dy2
+        )
+        return relativeReflectedCubicCurve
+    }
+
+    fun buildReflectedQuadCurve(x: Float, y: Float): PathNode {
+        val reflectedQuad = PathNode.ReflectiveQuadTo(
+            x = x,
+            y = y
+        )
+        return reflectedQuad
+    }
+
+    fun buildRelativeReflectedQuadCurve(dx: Float, dy: Float): PathNode {
+        val relativeReflectedQuad = PathNode.RelativeReflectiveQuadTo(
+            dx = dx,
+            dy = dy
+        )
+        return relativeReflectedQuad
+    }
+}
--- androidsvg/src/main/java/com/kbomeisl/androidsvg/parsing/SvgParser.kt
+++ androidsvg/src/main/java/com/kbomeisl/androidsvg/parsing/SvgParser.kt
@@ -1,94 +1,87 @@
 package com.kbomeisl.androidsvg.parsing
 
 import android.content.Context
+import android.util.Xml
 import com.kbomeisl.androidsvg.parsing.models.Tag
 import org.w3c.dom.Document
 import org.w3c.dom.Element
 import org.w3c.dom.Node
 import org.w3c.dom.NodeList
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserException
+import org.xmlpull.v1.XmlPullParserFactory
 import java.io.BufferedReader
+import java.io.IOException
 import java.io.InputStream
+import java.security.KeyStore.Entry
 import javax.xml.parsers.DocumentBuilderFactory
 import javax.xml.xpath.XPathConstants
 import javax.xml.xpath.XPathFactory
 
 class SvgParser(private val context: Context) {
+    private val ns: String? = null
 
-    fun parseSvg(fileName: String): List<Any?> {
-        val domBuilder = DomBuilder(context)
-        val dom = domBuilder.getDocumentFromAssets(fileName)
-        val nodeList = getNodes(dom = dom, tag = "path")
-        val nodeData = parseNodeList(nodeList)
-        return nodeData
+    val pullParserFactory = XmlPullParserFactory.newInstance()
+    val parser = pullParserFactory.newPullParser()
+
+    @Throws(IOException::class, XmlPullParserException::class)
+    fun parse(inputStream: InputStream) {
+        inputStream.use { iStream ->
+            val parser: XmlPullParser = Xml.newPullParser()
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false)
+            parser.setInput(iStream, null)
+            parser.nextTag()
+            return parseSvg(parser)
+        }
     }
 
-    fun getNodes(dom: Document, tag: String, attribute: String? = null): NodeList {
-        val nodeDataList = mutableListOf<Any?>()
-        return dom.getElementsByTagName("path") as NodeList
-    }
-
-    fun getStringExpression(
-        tag: String,
-        id: String? = null,
-        className: String? = null,
-        attribute: String? = null
-    ): String {
-        val stringBuilder = StringBuilder("//${tag}")
-        if (id != null) {
-            stringBuilder.append("[@id='$id']")
-        }
-        if (className != null) {
-            stringBuilder.append("[@class='$className']")
-        }
-        if (attribute != null) {
-            stringBuilder.append("/@$attribute")
-        }
-        return stringBuilder.toString()
-    }
-
-    fun parseNodeList(
-        nodeList: NodeList
-    ): List<Any?> {
-        val nodeDataList = mutableListOf<String>()
-        for (n in 0..nodeList.length-1) {
-            val attributes = nodeList.item(n).attributes
-            for (n in 0..attributes.length-1) {
-                nodeDataList.add(attributes.item(n).nodeValue)
+    @Throws(IOException::class, XmlPullParserException::class)
+    fun parseSvg(parser: XmlPullParser) {
+        val paths = mutableListOf<String>()
+        parser.require(XmlPullParser.START_TAG, null, "svg")
+        while (parser.next() != XmlPullParser.END_TAG) {
+            val fill = parser.getAttributeValue(null,"fill")
+            val height = parser.getAttributeValue(null, "height")
+            val width = parser.getAttributeValue(null, "width")
+            if (parser.eventType != XmlPullParser.START_TAG) {
+                continue
+            }
+            if (parser.name == "path") {
+                paths.add(parsePath(parser))
             }
         }
-        return nodeDataList
     }
 
-//    fun processElementNode(elementNode: Node): String? {
-//        if (elementNode.hasChildNodes()) {
-//            parseNodeList(elementNode.childNodes)
-//        }
-//        if (elementNode.hasAttributes()) {
-//            processAttributeNode(elementNode)
-//        }
-//        return elementNode.nodeValue
-//    }
+    @Throws(IOException::class, XmlPullParserException::class)
+    fun parsePath(parser: XmlPullParser): String {
+        parser.require(XmlPullParser.START_TAG,null, "path")
+        val path = parseText(parser)
+        parser.require(XmlPullParser.END_TAG,null,"path")
+        return path
+    }
 
-//    fun processTextNode(textNode: Node): String {
-//        return textNode.textContent
-//    }
+    @Throws(IOException::class, XmlPullParserException::class)
+    fun parseText(parser: XmlPullParser): String {
+        var text = ""
+        if (parser.next() == XmlPullParser.TEXT) {
+            text = parser.text
+            parser.nextTag()
+        }
+        return text
+    }
 
-//    fun processAttributeNode(
-//        attributeNode: Node
-//    ): List<Pair<String, String?>> {
-//        val attributes = mutableListOf<Pair<String, String?>>()
-//        if (attributeNode.hasAttributes()) {
-//            val attribute = attributeNode.attributes
-//            attributeList.forEach {
-//                val attrKey = it
-//                val attrValue = attribute.getNamedItem(it)?.nodeValue
-//                attributes.add((attrKey to attrValue))
-//            }
-//            if (attributeNode.hasChildNodes()) {
-//                parseNodeList(attributeNode.childNodes)
-//            }
-//        }
-//        return attributes.toList()
-//    }
+    @Throws(IOException::class, XmlPullParserException::class)
+    fun skipTag(parser: XmlPullParser) {
+        if (parser.eventType != XmlPullParser.START_TAG) {
+            throw IllegalStateException()
+        }
+        var depth = 1
+        while (depth != 0) {
+            when (parser.next()) {
+                XmlPullParser.END_TAG -> depth--
+                XmlPullParser.START_TAG -> depth++
+            }
+        }
+    }
 
 }
